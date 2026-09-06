3cbd33be659c660e25e6873b218dbfc7ceb7a72f
BB-Simon <simon.chowdery@gmail.com>
2026-09-07 02:51:25 +0300

    Implement currency model

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,18 @@
 class Currency < ApplicationRecord
+  has_many :wallets, dependent: :restrict_with_error
+
+  before_validation :normalize_code
+
+  validates :code, presence: true, uniqeness: true
+  validates :name, presence: true
+  validates :decimal_places, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0
+  }
+
+
+  private
+  def normalize_code
+    self.code = code.to_s.strip.upcase
+  end
 end
